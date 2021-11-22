a80099d1659d42d9129e929832d8d4cfa80fc241
TranDanh1122 <trandanh14042000@gmail.com>
2021-11-23 06:46:24 +0700

    update payment form

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::post('/editbai/{id}', 'Mua_Ban_Controller@edit_mathang')->name('suabai');
 Route::post('/luuanh', 'Mua_Ban_Controller@luuanh');
 Route::post('/cmt', 'Mua_Ban_Controller@savecomment');
 Route::post('/danhgia', 'Mua_Ban_Controller@savedanhgia');
+Route::get('/payment', 'Mua_Ban_Controller@loadpay');
+Route::post('/payment-reponse', 'Mua_Ban_Controller@savepay')->name('payment');
